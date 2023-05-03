a08f36a7f383be90e91a02609e509c2ad818e6ab
André Bueno <andrebueno99@gmail.com>
2023-05-03 15:05:34 -0300

    Troca de palavras JAVASCRIPT

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello!');
+const palavras = ["PESSOAS", "AMIGOS", "VOCÊ", "FAMÍLIAS"];
+let SELECT = 0;
+setInterval(function () {
+    
+    if (SELECT <= 0 || SELECT < 3)
+    {
+        SELECT += 1;
+    }else if (SELECT == 3)
+    {
+        SELECT = 0;
+    }
+   console.log(SELECT);
+    document.getElementById("textChanger").innerHTML = palavras[SELECT];
+}, 1000);
+
+
