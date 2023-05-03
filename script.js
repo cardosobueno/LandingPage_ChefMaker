4ea6dabe27eb4d74efe0a359e341a0c8c4ab44bb
André Bueno <andrebueno99@gmail.com>
2023-05-03 15:46:17 -0300

    Fade JQuery e arquivos renomeados

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ console.log('Hello!');
 const palavras = ["PESSOAS", "AMIGOS", "VOCÊ", "FAMÍLIAS"];
 let SELECT = 0;
 setInterval(function () {
-    
-    if (SELECT <= 0 || SELECT < 3)
-    {
-        SELECT += 1;
-    }else if (SELECT == 3)
-    {
-        SELECT = 0;
+    if (SELECT <= 0 || SELECT < 3) {
+      SELECT += 1;
+    } else if (SELECT == 3) {
+      SELECT = 0;
     }
-   console.log(SELECT);
-    document.getElementById("textChanger").innerHTML = palavras[SELECT];
-}, 1000);
+    console.log(SELECT);
+    $("#textChanger").fadeOut(function() {
+      $(this).html(palavras[SELECT]).fadeIn();
+    });
+  }, 5000);
 
-
+//document.getElementById("textChanger").innerHTML = palavras[SELECT];
