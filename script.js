82074e8e5f1337c6fdaf097c00ce0ebb3e89d436
André Bueno <andrebueno99@gmail.com>
2023-05-09 13:37:06 -0300

    Página em breve, botão deslizante, botao Instagram

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 
 console.log('Hello!');
-const palavras = ["PESSOAS", "AMIGOS", "VOCÊ", "FAMÍLIAS"];
-let SELECT = 0;
-setInterval(function () {
-  if (SELECT <= 0 || SELECT < 3) {
-    SELECT += 1;
-  } else if (SELECT == 3) {
-    SELECT = 0;
-  }
-  console.log(SELECT);
-  $("#textChanger").fadeOut(function () {
-    $(this).html(palavras[SELECT]).fadeIn();
-  });
-}, 5000);
+function fodas()
+{
+  const palavras = ["PESSOAS", "AMIGOS", "VOCÊ", "FAMÍLIAS"];
+  let SELECT = 0;
+  setInterval(function () {
+    if (SELECT <= 0 || SELECT < 3) {
+      SELECT += 1;
+    } else if (SELECT == 3) {
+      SELECT = 0;
+    }
+    console.log(SELECT);
+    $("#textChanger").fadeOut(function () {
+      $(this).html(palavras[SELECT]).fadeIn();
+    });
+  }, 5000);
+}
 
-
+fodas();
 
 
 
